71319a22168da5b431d77413e9cbf524de36aa29
Allison Falls <allison.falls2@gmail.com>
2021-07-29 17:07:38 -0400

    practice

--- src/HelloPolaris.java
+++ src/HelloPolaris.java
@@ -4,6 +4,35 @@ public class HelloPolaris {
 
         System.out.println("Hello There World!!!");
 
+// here's a comment
+
+        //     int newNumber;
+        //    newNumber = 5;
+
+        byte dayOfMonth = 31;
+
+        short employees = 129;
+
+        int carPrice = 5000;
+
+        long diameterOfBig = 457495734758454L;
+
+        float gradAverage = 94.56F;
+
+        String name = "Allison Falls";
+
+        String someQuote = "\"Believe everything you see on the internet\" \n - Abraham Lincoln";
+
+
+        System.out.println(someQuote);
+
+        final int SIDES_OF_A_DICE = 6;
+        final String API_KEY = "b75b703d8195f6f433ca";
+        final String GITHUB_API_BASE_URL = "https://api.github.com";
+
+        int multiply = 5 * 6;
+
+        double divide = 10 / 3f;
     }
 
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,35 @@ public class HelloWorld {
         System.out.print("Hello, ");
         System.out.print("World!");
 
+
+        int myFavoriteNumber = 24;
+        System.out.print(myFavoriteNumber);
+
+        char myChar = 'a';
+
+        String myString = "3.145";
+        System.out.print(myString);
+
+        float myNumber = 123F;
+        System.out.print(myNumber);
+
+      //  int x = 5;
+      //  System.out.println(++x);
+     //   System.out.println(x);
+
+      //  boolean class;
+       // class = false;
+
+        int x = 4;
+         x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//         y += x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
     }
 }
