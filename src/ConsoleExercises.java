c3879144abec3e3665594c988e88f3a576eb2316
Allison Falls <allison.falls2@gmail.com>
2021-08-02 11:21:57 -0400

    finished scanner class

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,6 +5,26 @@ public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
 
-        System.out.println("The value of pi is approximately \"" + pi + "\" ");
+        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter something: ");
+//        Number userInput = scanner.nextInt();
+//
+//        System.out.println("The number is:  \"" + userInput + "\" .");
+//
+//        System.out.print("Enter three words: ");
+//        String wordInput1 = scanner.next();
+//        String wordInput2 = scanner.next();
+//        String wordInput3 = scanner.next();
+//
+//        System.out.println("You entered: --> \"" + wordInput1 + wordInput2 + wordInput3 + "\" <--");
+
+//        System.out.print("Enter a sentence: ");
+//        String sentInput = scanner.nextLine();
+//
+//        System.out.println("The sentence is: \"" + sentInput + "\" ");
+
+
+
     }
 }
