502c62d08c5a1c24103f3509189cc5dce6fbd61a
Allison Falls <allison.falls2@gmail.com>
2021-08-03 12:18:09 -0400

    finished string basics

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,86 +1,133 @@
 import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//int i = 5;
-//        while (i <= 15) {
-//            System.out.println(i);
-//            i++;
-//        }
-//
-//    int counting = 0;
-//
-//    do{
-//        System.out.println(counting);
-//        counting += 2;
-//
-//    }while (counting <= 100);
-
-//        int counting = 100;
-////
-////        do{
-////            System.out.println(counting);
-////            counting -= 5;
-////
-////        }while (counting >= -10);
 
 
-//        long counting = 2L;
+//    int i = 5;
 //
-//        do{
-//            System.out.println(counting);
-//            counting *= counting;
-//            counting = counting * counting;
-//        }while (counting < 1000000L);
+//    while (i <= 15) {
+//        System.out.print(i + " ");
+//        i++;
+//    }
 
-//        for (int i = 100; i >= -10; i -= 5){
+
+//    int counter = 0;
+//
+//    do {
+//        System.out.println(counter);
+//        counter += 2;
+//        counter = counter + 2;
+//    }while (counter <= 100);
+
+//      int backwardsCounter = 100;
+//
+//      do {
+//          System.out.println(backwardsCounter);
+//          backwardsCounter -= 5;
+//
+//      }while (backwardsCounter >= -10);
+
+//        for (int i = 100; i >= -10; i -= 5) {
 //            System.out.println(i);
 //        }
 
-//        for (long i = 2L; i < 1000000L; i *= 1){
+//        long twoCounter = 2L;
+//
+//        do {
+//            System.out.println(twoCounter);
+//            twoCounter *= twoCounter;
+//            twoCounter = twoCounter * twoCounter;
+//        }while (twoCounter < 1000000L);
+
+//        for (long i = 2L; i < 1000000L; i *= i){
 //            System.out.println(i);
 //        }
+
 
 //        for (int i = 1; i <= 100; i++){
-//            if(i % 5 == 0 && i % 3 == 0){
+//            if (i % 5 == 0 && i % 3 == 0){
 //                System.out.println("FizzBuzz");
-//            }else if (i % 5 == 0) {
+//            } else if (i % 5 == 0) {
 //                System.out.println("Buzz");
-//            }else if (i % 3 == 0) {
+//            } else if (i % 3 == 0) {
 //                System.out.println("Fizz");
-//            }else {
+//            } else {
 //                System.out.println(i);
 //            }
-//         }
-
 //
+//        }
+
+
+        Scanner scanner = new Scanner(System.in);
+//
+//        boolean userContinues = true;
+//
+//        do {
+//            System.out.println("What number would you like to go up to?");
+//            int userInt = scanner.nextInt();
+//            System.out.println("\nHere is your table!\n");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//            for (int i = 1; i <= userInt; i++) {
+//                System.out.printf("%-6d", i);
+//                System.out.print(" | ");
+//                System.out.printf("%-7d", i * i);
+//                System.out.print(" | ");
+//                System.out.print(i * i * i);
+//                //Docrob alternate table display
+////                System.out.printf("%-6d | %-7d | %-5d\n", i, i*i, i*i*i);
+//                System.out.println();
+//            }
+//            System.out.println("Would you like to enter another number (y/n)?");
+//            String userResponse = scanner.next();
+//            if (!userResponse.equalsIgnoreCase("y")){
+//                userContinues = false;
+//            }
+//
+//
+//        }while(userContinues);
 
 
         boolean anotherGrade = true;
 
-        do{
-            System.out.println("Please enter a letter grade from 0 - 100");
+        do {
+            System.out.println("Please enter a numerical grade from 0 - 100");
 
             int userGrade = scanner.nextInt();
 
-            if (userGrade >= 88){
+            if (userGrade >= 88) {
                 System.out.println("A");
             } else if (userGrade >= 80) {
                 System.out.println("B");
-            }else if (userGrade >= 67){
+            } else if (userGrade >= 67) {
                 System.out.println("C");
-            }else if (userGrade >= 60) {
+            } else if (userGrade >= 60) {
                 System.out.println("D");
             } else {
                 System.out.println("F");
             }
 
-            System.out.println("Do you want to enter a new grade? (y/n");
+            System.out.println("Do you want to enter a new grade? (y/n)?");
 
             String userResponse = scanner.next();
 
 
+            if (!userResponse.equalsIgnoreCase("y")){
+                anotherGrade = false;
+            }
+
 
         }while (anotherGrade);
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/StringExercise.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class StringExercise {
+    public static void main(String[] args) {
+
+//        String input = "We don't need no education\n" +
+//                "We don't need no thought control";
+//        System.out.println(input);
+//
+//        String input2 = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(input2);
+//
+//        String input3 = "In windows, the main drive is usually C:\\";
+//        System.out.println(input3);
+//
+//        String input4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+//                "and the amazing triple backslash \\\\\\!";
+//        System.out.println(input4);
+
+
+    }
+}
