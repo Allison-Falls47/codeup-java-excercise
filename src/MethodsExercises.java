7a0bc71d9436eeae65b9a5b097f12f56d448f281
Allison Falls <allison.falls2@gmail.com>
2021-08-04 11:49:22 -0400

    finished getInteger

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,31 +1,52 @@
+import java.util.Scanner;
 public class MethodsExercises {
 
-    public static int addition(int a, int b) {
-        return a + b;
-    }
+//    public static int addition(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public static int subtraction(int a, int b) {
+//        return a - b;
+//    }
+//
+//    public static int multiplication(int a, int b) {
+//        return a * b;
+//    }
+//
+//    public static int division(int a, int b) {
+//        return a / b;
+//    }
+//
+//    public static int modulus(int a, int b) {
+//        return a % b;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(addition(4, 5));
+//        System.out.println(subtraction(7, 4));
+//        System.out.println(multiplication(7, 4));
+//        System.out.println(division(20, 4));
+//        System.out.println(modulus(2, 3));
+//    }
 
-    public static int subtraction(int a, int b) {
-        return a - b;
-    }
+    public static int getInteger(int min, int max){
 
-    public static int multiplication(int a, int b) {
-        return a * b;
-    }
+     Scanner input = new Scanner(System.in);
 
-    public static int division(int a, int b) {
-        return a / b;
-    }
+     System.out.print("enter a number between " + min + " - " + max + ": ");
 
-    public static int modulus(int a, int b) {
-        return a % b;
+     int response = input.nextInt();
+
+     if (response >= min && response <= max){
+         return response;
+     }else{
+         System.out.println(response + " is not within range.");
+         return getInteger(min, max);
+     }
     }
 
     public static void main(String[] args) {
-        System.out.println(addition(4, 5));
-        System.out.println(subtraction(7, 4));
-        System.out.println(multiplication(7, 4));
-        System.out.println(division(20, 4));
-        System.out.println(modulus(2, 3));
+        System.out.println(getInteger(1, 10));
     }
 
 
