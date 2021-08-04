afbdb0e71bca4f81462865022447b494714fdd30
Allison Falls <allison.falls2@gmail.com>
2021-08-04 10:23:30 -0400

    Basic Arithmetics done

---
+++ src/MethodsExercises.java
@@ -0,0 +1,29 @@
+public class MethodsExercises {
+
+//    public static int addition(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public static int subtraction(int a, int b) {
+//        return a - b;
+//    }
+//
+//    public static int multiplication(int a, int b) {
+//        return a * b;
+//    }
+//
+//    public static int divison(int a, int b) {
+//        return a / b;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(addition(4, 5));
+//        System.out.println(subtraction(7, 4));
+//        System.out.println(multiplication(7, 4));
+//        System.out.println(divison(20, 4));
+//    }
+
+
+
+
+}
