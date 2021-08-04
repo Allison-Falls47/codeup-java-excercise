73e188e666b8d354bb02201ba9a30d355c1dd748
Allison Falls <allison.falls2@gmail.com>
2021-08-04 10:26:09 -0400

    added modulus

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,27 +1,32 @@
 public class MethodsExercises {
 
-//    public static int addition(int a, int b) {
-//        return a + b;
-//    }
-//
-//    public static int subtraction(int a, int b) {
-//        return a - b;
-//    }
-//
-//    public static int multiplication(int a, int b) {
-//        return a * b;
-//    }
-//
-//    public static int divison(int a, int b) {
-//        return a / b;
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println(addition(4, 5));
-//        System.out.println(subtraction(7, 4));
-//        System.out.println(multiplication(7, 4));
-//        System.out.println(divison(20, 4));
-//    }
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static int division(int a, int b) {
+        return a / b;
+    }
+
+    public static int modulus(int a, int b) {
+        return a % b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addition(4, 5));
+        System.out.println(subtraction(7, 4));
+        System.out.println(multiplication(7, 4));
+        System.out.println(division(20, 4));
+        System.out.println(modulus(2, 3));
+    }
 
 
 
