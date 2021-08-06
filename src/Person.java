e4190a12f761831eaa8d90c1fa39896da7d66dd6
Allison Falls <allison.falls2@gmail.com>
2021-08-06 11:56:07 -0400

    object oriented finished

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,10 @@
 public class Person {
         private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
     public String getName(){
         return name;
     }
@@ -7,6 +12,17 @@ public class Person {
         this.name = name;
     }
     public void sayHello(){
-        System.out.println("hello" + this.name);
+        System.out.println("hello " + name);
+    }
+
+
+    public static void main(String[] args) {
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 }
---
+++ src/util/Input.java
@@ -0,0 +1,62 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+    public String getString() {
+        System.out.println("Type here:");
+        String userString = scanner.nextLine();
+        return userString;
+    }
+
+    public boolean yesNo(){
+        System.out.println("(Y/N) ");
+        String userAns = scanner.nextLine();
+        return userAns.equals("y");
+    }
+    public int getInt(int min, int max){
+        System.out.println("Enter a integer: ");
+        int userInt = scanner.nextInt();
+
+        if (userInt < min || userInt > max){
+            System.out.println("Try again!");
+            return getInt(min, max);
+        }
+        else{
+            System.out.println("Valid integer");
+            return userInt;
+        }
+    }
+
+    public int getInt(){
+        System.out.println("Type an integer: ");
+        int userInt = scanner.nextInt();
+        return userInt;
+    }
+
+    public double getDouble(double min, double max){
+        System.out.println("Enter a integer: ");
+        double userDouble = scanner.nextDouble();
+
+        if (userDouble < min || userDouble > max){
+            System.out.println("Try again!");
+            return getDouble(min, max);
+        }
+        else{
+            System.out.println("Valid decimal");
+            return userDouble;
+        }
+    }
+
+    public double getDouble(String prompt){
+        System.out.println(prompt);
+        double userDouble = scanner.nextDouble();
+        return userDouble;
+    }
+
+}
