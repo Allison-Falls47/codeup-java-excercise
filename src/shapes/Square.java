8f279bd298c5b860f8358393f54d38151a19673a
Allison Falls <allison.falls2@gmail.com>
2021-08-09 11:36:06 -0400

    fixed overdrive

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,7 +2,19 @@ package shapes;
 
 public class Square extends Rectangle {
 
-    public Square(int length, int width) {
-        super(length, width);
+    public Square (int side) {
+       super(side, side);
     }
+
+@Override
+    public int getArea() {
+    System.out.println("overdrive area");
+        return super.length * super.width;
+    }
+@Override
+    public int getPerimeter() {
+        System.out.println("overdrive perimeter");
+        return 4 * super.length;
+    }
+
 }
