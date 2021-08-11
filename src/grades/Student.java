ef4dbb6243d7ea8cb317f3d68a0df4dd781dcdc0
Allison Falls <allison.falls2@gmail.com>
2021-08-11 12:37:47 -0400

    grade application

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,20 +1,26 @@
 package grades;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class Student {
     private String name;
     private final ArrayList<Integer> grades = new ArrayList<>();
 
-//    public Student(String name, Integer grades) {
-//        this.name = name;
-//        this.grades.isEmpty();
-//    }
-    // returns the student's name
-    public String getName();
-    // adds the given grade to the grades property
-    public void addGrade(int grade);
-    // returns the average of the students grades
-    public double getGradeAverage();
+    public Student(String name, Integer grades) {
+        this.name = name;
+        this.grades = grades.isEmpty();
+    }
+    public static void main(String[] args) {
+        // returns the student's name
+        public String getName();
+
+        // adds the given grade to the grades property
+        public void addGrade(int grade);
+        // returns the average of the students grades
+        public double getGradeAverage();
+    }
 }
 
